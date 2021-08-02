d6e064505cf0831d8460159ebc4110e408a06d1c
Amal <87772836+amaldeeps7@users.noreply.github.com>
2021-08-02 21:12:38 +0530

    Create HelloWorld.c

---
+++ C-Programming/HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n\n\t\tHello world\n\n\n"); 
+    getchar();
+    getchar();
+    return 0;
+}
+//printf function is used to print data on to screen
+//\n is uded to move control on to next line
+//\t is used to give a horizontal tab
