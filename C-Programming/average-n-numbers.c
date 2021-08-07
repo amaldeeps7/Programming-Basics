9c8cec7723cb43aceab9e0dad4204194facaf55a
Amal <87772836+amaldeeps7@users.noreply.github.com>
2021-08-07 20:24:25 +0530

    Add files via upload

---
+++ C-Programming/average-n-numbers.c
@@ -0,0 +1,21 @@
+//Program to find average of n numbers
+#include<stdio.h>
+
+int main()
+{
+    int n, i;
+    float sum = 0, x;
+
+    printf("Enter number of elements:  ");
+    scanf("%d", &n);
+    printf("\n\n\nEnter %d elements\n\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%f", &x);
+        sum += x;
+    }
+    printf("\n\n\nAverage of the entered numbers is =  %f", (sum/n));
+    getchar();
+    getchar();
+    return 0;
+}
